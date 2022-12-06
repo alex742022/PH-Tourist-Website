8985d6af56415617eefd91c229835c00bb05c7a2
alex742022 <alexis.aquino.lumbera7@gmail.com>
2022-12-06 14:26:45 +0800

    change some content

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-export default function Header(props){
 
+export default function Header(props){
+    
     return(
+
         <div className="header-container">
             <h2>Top 10 |</h2>
             <h1>Tourist Spot in Philippines</h1>
