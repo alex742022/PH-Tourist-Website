83f8793198615db3c87a28e8440fa188603a34d6
alex742022 <alexis.aquino.lumbera7@gmail.com>
2022-12-06 14:26:03 +0800

    change alt img text

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
     <footer>
       <div className="img-container">
         <div className="overlay-footer"></div>
-        <img src={FooterPc} alt="Footer Image" />
+        <img src={FooterPc} alt="Footer Img" />
         <div className="text-footer-container">
           <h1>WHAT ARE YOU WAITING FOR?</h1>
           <p>Travel and Explore!</p>
